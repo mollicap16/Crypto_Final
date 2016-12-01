3888c482a3cc8dc6da18319edf1eee4061265894
Robert Irwin <robert.irwin@temple.edu>
2016-12-01 12:25:43 -0500

    Tested module file

--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
+from modules.comm_init import *
 # main script here
+
+def main():
+    sender_init("192.168.0.1", 5005)
+    receiver_init("192.168.0.2", 8888)
+
+
+if __name__ == "__main__":
+    main()
+    
--- src/modules/comm_init.py
+++ src/modules/comm_init.py
@@ -1,5 +1,9 @@
 def sender_init(IP, PORT):
     # initialize TCP socket for sender
+    print IP
+    print PORT
 
 def receiver_init(IP, PORT):
     # initialize TCP socket for receiver
+    print IP
+    print PORT
